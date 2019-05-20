70adad9c1390c72221ec9d0138cd40f1523e6410
Dmitry Kolinchuk <dmitry.kolinchuk@sitecore.com>
2019-05-20 11:23:39 +0300

    Display forms when the IndexAllFields setting is set to false

---
+++ src/Sitecore.Support.229445/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.229445")]
+[assembly: AssemblyProduct("Sitecore.Support.229445")]
+[assembly: ComVisible(false)]
